c93ce4319a5ffc96a56a7c5c8bddd499439f1d76
Michel Dismer <michel.dismer@gmail.com>
2025-10-12 09:05:40 +0000

    dnf update

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,7 @@ dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/g
 rpm --import https://packages.microsoft.com/keys/microsoft.asc 
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-dnf5 check-update
+dnf5 update -y
 
 dnf5 install -y code
 dnf5 install gh -y --repo gh-cli
