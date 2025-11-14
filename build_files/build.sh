2158519f503f755a79eedc0cd9cd02680aa4c6a7
Michel Dismer <michel.dismer@gmail.com>
2025-11-14 17:15:31 +0000

    remove gh add unity

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,21 +9,19 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+sh -c 'echo -e "[unityhub-beta]\nname=Unity Hub Beta\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/unstable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/unstable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub_beta.repo'
+
 dnf5 -y install dnf-plugins-core
 dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
 rpm --import https://packages.microsoft.com/keys/microsoft.asc 
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 
 dnf5 install -y code
-dnf5 install gh -y --repo gh-cli
 dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
 dnf5 install -y nodejs
+dnf5 install -y unityhub
 
 # Use a COPR Example:
 #
